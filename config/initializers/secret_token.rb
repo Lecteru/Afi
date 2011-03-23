8efe56ad85b32c9ef2038f6637191ee2f52e3b8d
Dmitriy Kulakov <lecteru@gmail.com>
2011-03-23 13:05:36 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Afi::Application.config.secret_token = 'cee01b50de5f0e2965d1c27b6c30b97ed756f3f8e2786df610bfbcc2eb558664cb3315d8cb6b70ecaf317b6664ca5c699b86ac1f2ae1c719e8ff204f8a911fff'
