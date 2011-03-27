6a0aae7689a9de2e73afd62a4ac9d0e3555b81e5
Aleksander Kozhukhovskiy <kozhukhovskiy@gmail.com>
2011-03-27 18:51:39 +0800

    create view for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,20 @@
 class EventsController < ApplicationController
+  def new
+    @event = Event.new
+  end
+  
+  def create
+    @event = Event.new(params[:event])
+  
+    if @event.save
+      flash[:notice] = 'Event was successfully created.'
+      redirect_to(@event) 
+    else
+      render :action => "new" 
+    end
+  end
+
+  def index
+    @events = Event.all
+  end
 end
