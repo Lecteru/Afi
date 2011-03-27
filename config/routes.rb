0a458567d7ba599619433e7db72b34d6cf4b5da1
Aleksander Kozhukhovskiy <kozhukhovskiy@gmail.com>
2011-03-27 18:18:21 +0800

    install haml, add events, change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Afi::Application.routes.draw do
   resources :microposts
 
   resources :users
+  resources :events
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +53,7 @@ Afi::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "events#index"
 
   # See how all your routes lay out with "rake routes"
 
